5c63366820e97049357d1cc8b7b6a227cf25dfbf
mmelucci <mmelucci20@stuy.edu>
2020-11-21 22:49:33 -0500

    Added the add method and its tests

--- RealRationalTester.java
+++ RealRationalTester.java
@@ -3,6 +3,7 @@ public class RealRationalTester {
     RealNumber a = new RealNumber(0.0);
     RealNumber b = new RealNumber(1.0);
     RealNumber c = new RealNumber(1.000009);
+    System.out.println("Equals tests:");
     System.out.println("Expected Result: false");
     System.out.print("Result: ");
     System.out.println(a.equals(b));
@@ -12,5 +13,16 @@ public class RealRationalTester {
     System.out.println("Expected Result: true");
     System.out.print("Result: ");
     System.out.println(b.equals(c));
+    System.out.println();
+    System.out.println("Add tests:");
+    System.out.println("Expected Result: 0.0");
+    System.out.print("Result: ");
+    System.out.println(a.add(a));
+    System.out.println("Expected Result: 1.0");
+    System.out.print("Result: ");
+    System.out.println(b.add(a));
+    System.out.println("Expected Result: 2.000009");
+    System.out.print("Result: ");
+    System.out.println(b.add(c));
   }
 }
